83440e96c4f658a562fda2c519b897844d9425e3
福飯　雄太 <yuta@fukuhanyuutanoMacBook-Air.local>
2022-03-06 16:27:07 +0900

    feat: migrateの作成

--- app/Models/TalkList.php
+++ app/Models/TalkList.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Review;
 
 class TalkList extends Model
 {
   //
   protected $fillable = ['content'];
+
+  public function reviews() {
+      return $this->hasMany(Review, 'talkList_id', 'id');
+  }
 }
